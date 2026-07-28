6899ef7aeebb6330903ed2501f2fef24ca8af484
GANA VT <visiontech988@gmail.com>
2026-07-28 11:08:22 +0800

    Force dynamic sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,11 +1,13 @@
 import { MetadataRoute } from "next";
 
+export const dynamic = "force-dynamic";
+
 const siteUrl = "https://xinfinityhub.vercel.app";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: siteUrl,
+      url: `${siteUrl}/`,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
